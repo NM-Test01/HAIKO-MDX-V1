70c9802b8997835347f92c92e48812fa082e1410
NM-Test01 <essoumasylvain4@gmail.com>
2025-05-29 14:05:51 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~GIASzS5T#tvQ4Iw6EY1SnmUgoUpemwVn1EEsxzXLxsQgCJhOCbf0",
   PREFIX: process.env.PREFIX || '+',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
